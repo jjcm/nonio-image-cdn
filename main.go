a636da66c7dee26e2fd34bede743e92822843b33
Jake Miller <j@jjcm.org>
2020-05-17 20:30:10 +1000

    made the uplaod require user authentication

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -13,6 +14,12 @@ type uploadResponse struct {
 	Name   string
 }
 
+type authorizationResponse struct {
+	Error string
+	Email string
+	ID    int
+}
+
 func uploadFile(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "OPTIONS" {
 		handlers.SendResponse(w, "", 200)
@@ -20,6 +27,19 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 	}
 	// Parse our multipart form, set a 1GB max upload size
 	r.ParseMultipartForm(1 << 30)
+
+	// Get the user's email if we're authorized
+	bearerToken := r.Header.Get("Authorization")
+	fmt.Println(bearerToken)
+	user, err := getUserEmail(bearerToken)
+	fmt.Println(user)
+	if err != nil {
+		fmt.Println("User is not authorized.")
+		fmt.Println(err)
+		res := uploadResponse{"User is not authorized", bearerToken}
+		handlers.SendResponse(w, res, 400)
+		return
+	}
 
 	// Parse our url, and check if the url is available
 	url := r.FormValue("url")
@@ -43,12 +63,12 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer file.Close()
-	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
-	fmt.Printf("File Size: %+v\n", handler.Size)
 
 	re, _ := regexp.Compile("([a-zA-Z]+)/")
 	var mimeType = handler.Header["Content-Type"][0]
-	fmt.Println(re.FindStringSubmatch(mimeType)[1])
+
+	// If all is good, let's log what the hell is going on
+	fmt.Printf("%v is uploading a %v of size %v to %v", user, re.FindStringSubmatch(mimeType)[1], handler.Size, url)
 
 	switch re.FindStringSubmatch(mimeType)[1] {
 	case "image":
@@ -76,6 +96,44 @@ func checkIfURLIsAvailable(url string) (bool, error) {
 	}
 
 	return string(body) == "true", err
+}
+
+func getUserEmail(bearerToken string) (string, error) {
+	// Send a req to the api to get the email from our token, if it's valid
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://api.non.io/protected", nil)
+	req.Header.Add("Authorization", bearerToken)
+	userAuthRes, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error checking if the user is authorized")
+		fmt.Println(err)
+		return "", err
+	}
+	defer userAuthRes.Body.Close()
+
+	// Parse the body of the request once it comes back
+	body, err := ioutil.ReadAll(userAuthRes.Body)
+	if err != nil {
+		fmt.Println("Error parsing the body of the user auth check")
+		fmt.Println(err)
+		return "", err
+	}
+
+	// Create a authResponse struct, fill it with the parsed json values
+	authResponse := authorizationResponse{}
+	err = json.Unmarshal(body, &authResponse)
+	if err != nil {
+		fmt.Println("Error parsing the json of the user auth check")
+		fmt.Println(err)
+		return "", err
+	}
+
+	// Populate our error with the json response's error
+	if authResponse.Error != "" {
+		err = fmt.Errorf(authResponse.Error)
+	}
+
+	return authResponse.Email, err
 }
 
 func setupRoutes() {
